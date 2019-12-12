775971873f2480493ed47ccaf2db4bec48827483
ftvalentini <ft.valentini@gmail.com>
2019-12-12 12:06:28 -0300

    imita stylo bto

--- 05-EDA_all_tweets.R
+++ 05-EDA_all_tweets.R
@@ -1,16 +1,13 @@
 
 library(tidyverse)
-library(data.table)
 library(forcats)
-library(hrbrthemes)
-library(ggtech)
+
+
 
 p = "D://Data Science/Maestria UBA/github/dm-TwitterBolivia/"
 
 all_tweets = readr::read_delim(paste0(p,"data/working/all_tweets.csv"), delim=",") %>%
-  janitor::clean_names() 
-
-all_tweets = all_tweets %>%
+  janitor::clean_names() %>%
   select(-x1)
 
 # Summary ----
@@ -46,12 +43,10 @@ ylim1 = boxplot.stats(all_tweets$tiempo_user)$stats[c(1, 5)]
 
 # scale y limits based on ylim1
 (p1 = p0 + coord_cartesian(ylim = ylim1*1.05) +
-  ggtitle("Antigüedad de las cuentas de Twitter") + 
-  scale_x_discrete(labels=c("Anti Evo","Pro Evo","Indefinido")) + 
-  ylab("Dias de antigüedad") + 
-  xlab("") + 
+  ggtitle("AntigÃ¼edad de las cuentas de Twitter") +
+  scale_x_discrete(labels=c("Anti Evo","Pro Evo","Indefinido")) +
+  ylab("Dias de antigÃ¼edad") +
+  xlab("") +
     theme_minimal(base_size = 15))
 
 ## Export
-
-  
